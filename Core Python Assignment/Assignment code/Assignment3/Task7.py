597c73f1a25e56c2a3748b02f1b18c6fb1271caa
NEHA SONAWANE <sonawaneneha127@gmail.com>
2025-09-19 10:54:47 +0530

    Delete Core Python Assignment/Assignment code directory
    delete message

--- Core Python Assignment/Assignment code/Assignment3/Task7.py
+++
@@ -1,7 +0,0 @@
-# Write a program to check if user has entered correct userid and password.
-user_id = input("Enter user ID:")
-password = input("Enter password:")
-if user_id == "Neha" and password == "Neha2110":
-    print("Loging succesful")
-else:
-    print("Invalid user ID or password")
--- Core Python Assignment/Assignment code/Assignment4/Task4.py
+++
@@ -1,6 +0,0 @@
-# WAP to print factorial of a number.
-n = int(input("Enter a number:"))
-fact = 1
-for i in range(1, n + 1):
-    fact *= i
-    print("Factorial of", n, "is", fact)
